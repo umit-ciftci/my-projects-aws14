7427ac600b379f687d506bf7bcf11163e769027d
Ümit <ciftciumit42@gmail.com>
2023-12-08 14:10:49 +0300

    tst

--- flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
+++ flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
@@ -38,7 +38,7 @@ VALUES
 	("Fatih", "fatih@porche.com"),
     ("Zeynep", "zeynep@tesla.com"),
     ("Sekure", "sekure@tesla.com"),
-    ("Sümeyye", "sümeyyeeee@samsung.com");
+    ("Sümeyye", "sümeyyeeeeee@samsung.com");
 """
 cursor.execute(drop_table)
 cursor.execute(users_table)
