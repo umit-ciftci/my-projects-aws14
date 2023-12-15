ef995f2f16238e9d7d66225fc324dd567ce5f1bd
Ümit <ciftciumit42@gmail.com>
2023-12-15 17:57:32 +0300

    tst

--- flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
+++ flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
@@ -36,7 +36,7 @@ VALUES
     ("Serafettin", "serafettin@bmw.com"),
     ("Murat", "murat@mercedes.com"),
 	("Fatih", "fatih@porche.com"),
-    ("Zeynep", "zeyneppp@tesla.com"),
+    ("Zeynep", "zeynepppp@tesla.com"),
     ("Sekure", "sekure@tesla.com"),
     ("Sümeyye", "sümeyye@samsung.com");
 """
