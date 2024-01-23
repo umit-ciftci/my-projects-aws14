a0384c8ccb6aed8a9705c1025de34cd9e087ff9c
Ümit <ciftciumit42@gmail.com>
2024-01-23 14:58:45 +0300

    tst

--- flask-05-Handling-SQL-with-Flask-Web-Application/app-with-sqlite.py
+++ flask-05-Handling-SQL-with-Flask-Web-Application/app-with-sqlite.py
@@ -22,7 +22,7 @@ with app.app_context():
         ("Murat", "murat@mercedes.com"),
 	    ("Fatih", "fatih@porche.com"),
         ("Zeynep", "zeynep@tesla.com"),
-        ("Sekure", "sekuuure@tesla.com"),
+        ("Sekure", "sekuuuuure@tesla.com"),
         ("Sümeyye", "sümeyye@samsung.com");
         """)
     db.session.execute(drop_table)
